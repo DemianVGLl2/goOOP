85b362ee021276b9ef5300ffb6bdce6a36c78e4e
DemianVGLl2 <0253139@up.edu.mx>
2026-02-16 07:53:40 -0600

    Start Golang modules
    Signed-off-by: DemianVGLl2 <0253139@up.edu.mx>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/DemianVGLl2/goOOP
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github/DemianVGLl2/goOOP/book"
+
+	"github.com/DemianVGLl2/goOOP/book"
+)
+
+func main() {
+	// Golang doesn't implement OOP
+	// Classes -> Structs
+	// Methods -> Methods over structures
+	// Encapsulation -> There is no modifiers such as private or public,
+	// but if we have a function with a capital letter it will be public, otherwise it will be private
+	// Inheritance -> Composition: Structures within structures
+	// Polymorphism -> Interfaces
+
+	var myBook = book.Book{
+		Title:  "Los cinco lenguajes del amor",
+		Author: "Gary Chapman",
+		Pages:  200,
+	}
+
+	myBook.PrintInfo()
+}
